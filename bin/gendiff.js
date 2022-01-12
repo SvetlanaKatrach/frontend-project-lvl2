0316dc934cc42c211619e5a5eea141d4f541a221
Svetlana Katrach <ssvetik@land.ru>
2022-01-12 09:57:29 +0400

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format');
 
-program.parse(process.argv)
+program.parse(process.argv);
